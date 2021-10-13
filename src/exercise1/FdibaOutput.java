490a0326fa2c8b766907de72e1f33b955a2286ca
Stilian <stildoge123@abv.bg>
2021-10-13 17:33:09 +0300

    helloMaMan

---
+++ src/exercise1/FdibaOutput.java
@@ -0,0 +1,11 @@
+package exercise1;
+
+public class FdibaOutput {
+    public void main(String[] args){
+        System.out.print("FFFFF DDDDD   II BBBBB      A\n" +
+                "F     D    D  II B    B    A A\n" +
+                "FFFF  D     D II BBBBB    AAAAA\n" +
+                "F     D    D  II B    B  A     A\n" +
+                "F     DDDDD   II BBBBB  A       A");
+    }
+}
